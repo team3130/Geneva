fdd2e17527423353236bafd51fe8bbebca643fc6
Duckfan77 <cwenthin@yahoo.com>
2016-02-07 15:46:16 -0600

    Updated Ports and removed unneeded Comment

--- src/Subsystems/Chassis.cpp
+++ src/Subsystems/Chassis.cpp
@@ -12,18 +12,10 @@ Chassis* Chassis::GetInstance()
 Chassis::Chassis() :
 		Subsystem("Chassis")
 {
-	m_drive = new RobotDrive(PORT_LEFTMOTOR,PORT_RIGHTMOTOR);
+	m_drive = new RobotDrive(CAN_LEFTMOTOR,CAN_RIGHTMOTOR);
 	m_drive->SetSafetyEnabled(false);
 	m_shifter = new Solenoid(PNM_GEARSHIFTER);
 	m_bShiftedLow = false;
-
-	// Inverted motors for TurboMoose wiring only.
-	/*
-	m_drive->SetInvertedMotor(RobotDrive::kFrontLeftMotor,true);
-	m_drive->SetInvertedMotor(RobotDrive::kFrontRightMotor,true);
-	m_drive->SetInvertedMotor(RobotDrive::kRearLeftMotor,true);
-	m_drive->SetInvertedMotor(RobotDrive::kRearRightMotor,true);
-	*/
 }
 
 void Chassis::InitDefaultCommand()
