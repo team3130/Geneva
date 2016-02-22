cee7f6a50adc534a40d232d96b888dfcd6e876fc
Duckfan77 <cwenthin@yahoo.com>
2016-02-22 16:11:14 -0600

    Flip Drive Direction
    Fixed the drive Direction

--- src/Commands/DefaultDrive.cpp
+++ src/Commands/DefaultDrive.cpp
@@ -21,8 +21,8 @@ void DefaultDrive::Initialize()
 void DefaultDrive::Execute()
 {
 	OI* oi = OI::GetInstance();
-	double moveSpeed = oi->stickL->GetY();
-	double turnSpeed = oi->stickR->GetX();
+	double moveSpeed = -oi->stickL->GetY();
+	double turnSpeed = -oi->stickR->GetX();
 
 	// Only driving manual should require Quadratic inputs. By default it should be turned off
 	// Therefore here we turn it on explicitly.
