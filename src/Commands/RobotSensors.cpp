00934fa95776255e94afc9828186f31d824bb4f2
Mikhail Kyraha <mikhail@kyraha.com>
2016-03-11 21:55:16 -0600

    Aiming Light on the DB
    Needs refactoring to optimize the calculations.

--- src/Commands/RobotSensors.cpp
+++ src/Commands/RobotSensors.cpp
@@ -1,6 +1,7 @@
 #include "RobotSensors.h"
 #include "Subsystems/Catapult.h"
 #include "Subsystems/Chassis.h"
+#include "Misc/Video.h"
 #include "OI.h"
 
 RobotSensors::RobotSensors()
@@ -23,8 +24,33 @@ void RobotSensors::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void RobotSensors::Execute()
 {
-	SmartDashboard::PutBoolean("DB/LED 0", Catapult::GetInstance()->isBottomHit());
-	SmartDashboard::PutBoolean("DB/LED 1", Chassis::GetInstance()->GetDistanceL()==0 or Chassis::GetInstance()->GetDistanceR()==0);
+	float turn = 0;
+	float dist = 0;
+	size_t nTurns = 0;
+
+	RobotVideo::GetInstance()->mutex_lock();
+	nTurns = RobotVideo::GetInstance()->HaveHeading();
+	if(nTurns > 0) {
+		turn = RobotVideo::GetInstance()->GetTurn(0);
+		dist = RobotVideo::GetInstance()->GetDistance(0);
+	}
+	if(fabs(turn) > 3.0 and nTurns > 1) {
+		turn = RobotVideo::GetInstance()->GetTurn(1);
+		dist = RobotVideo::GetInstance()->GetDistance(1);
+	}
+	RobotVideo::GetInstance()->mutex_unlock();
+
+	if (nTurns > 0) {
+		if (dist > 0) {
+			turn += atan2f(Preferences::GetInstance()->GetFloat("CameraOffset",RobotVideo::CAMERA_OFFSET), dist);
+		}
+		if (fabs(turn) < 0.4) SmartDashboard::PutBoolean("DB/LED 0", true);
+		else SmartDashboard::PutBoolean("DB/LED 0", false);
+	}
+	else SmartDashboard::PutBoolean("DB/LED 0", false);
+
+	SmartDashboard::PutBoolean("DB/LED 2", Catapult::GetInstance()->isBottomHit());
+	SmartDashboard::PutBoolean("DB/LED 3", Chassis::GetInstance()->GetDistanceL()==0 or Chassis::GetInstance()->GetDistanceR()==0);
 
 	std::ostringstream oss0; // Replace this one with something more important
 	oss0 << "Dist:" << Chassis::GetInstance()->GetDistance();
