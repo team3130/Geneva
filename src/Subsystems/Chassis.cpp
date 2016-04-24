a9d3ac3a49d4987eb4fd69c60cc510f4b705ab4d
Mikhail Kyraha <mikhail@kyraha.com>
2016-04-24 15:52:25 -0500

    ChassisWidth preference for angle measurement

--- src/Subsystems/Chassis.cpp
+++ src/Subsystems/Chassis.cpp
@@ -91,7 +91,7 @@ double Chassis::GetDistance()
 
 double Chassis::GetAngle()
 {
-	return 1.065 * ( GetDistanceR() - GetDistanceL() ) * 180 / (26.75 * M_PI);
+	return ( GetDistanceR() - GetDistanceL() ) * 180 / (Preferences::GetInstance()->GetDouble("ChassisWidth",28.55) * M_PI);
 	/*
 	 *  Angle is 180 degrees times encoder difference over Pi * the distance between the wheels
 	 *	Made from geometry and relation between angle fraction and arc fraction with semicircles.
