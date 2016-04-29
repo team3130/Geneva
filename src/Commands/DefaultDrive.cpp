cd18b5d0e3fc5c76e966b75513e9a1e3d3a98c95
Duckfan77 <cwenthin@yahoo.com>
2016-04-29 16:58:02 -0500

    Removed Throttle from drive.

--- src/Commands/DefaultDrive.cpp
+++ src/Commands/DefaultDrive.cpp
@@ -23,12 +23,12 @@ void DefaultDrive::Execute()
 	OI* oi = OI::GetInstance();
 	double moveSpeed = -oi->stickL->GetY();
 	double turnSpeed = -oi->stickR->GetX();
-	double moveThrottle = (-0.5 * OI::GetInstance()->stickL->GetRawAxis(3)) + 0.5;		//Get Slider Value
-	double turnThrottle = (-0.5 * OI::GetInstance()->stickR->GetRawAxis(3)) + 0.5;		//Get Slider Value
+//	double moveThrottle = (-0.5 * OI::GetInstance()->stickL->GetRawAxis(3)) + 0.5;		//Get Slider Value
+//	double turnThrottle = (-0.5 * OI::GetInstance()->stickR->GetRawAxis(3)) + 0.5;		//Get Slider Value
 
 	// Only driving manual should require Quadratic inputs. By default it should be turned off
 	// Therefore here we turn it on explicitly.
-	Chassis::GetInstance()->DriveArcade(moveSpeed * moveThrottle, turnSpeed * turnThrottle, true);
+	Chassis::GetInstance()->DriveArcade(moveSpeed, turnSpeed, true);
 }
 
 /// Make this return true when this Command no longer needs to run execute().
